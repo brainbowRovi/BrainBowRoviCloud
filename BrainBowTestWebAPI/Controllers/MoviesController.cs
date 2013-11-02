47e55a1371bfae1f4d602b1d2bd674b8ef9cae7f
rbtconsultants <rbtconsultants@gmail.com>
2013-11-02 11:44:57 -0400

    1st commit for cloud api

---
+++ BrainBowTestWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BrainBowTestWebAPI.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public  ActionResult GetMovieByKeyword(string keyword)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BrainBowTestWebAPI/Models/RoviAPI.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Helpers;
+using Newtonsoft.Json.Linq;
+
+namespace BrainBowTestWebAPI.Models
+{
+    public class RoviAPI
+    {
+        private const string RoviAppId = "7fk63kqh6es89qh75gvn6b9a";
+        private const string RoviAppSecretKey = "tPbdSg4AWG";
+
+        public JObject GetMovieByKeyword(string Keyword)
+        {
+            string OAuthSessionToken = GetAccessTokenFromCode( RoviAppId, RoviAppSecretKey );
+             return GetRovi_Metadata( OAuthSessionToken, Keyword);
+        }
+
+        private static JObject GetRovi_Metadata(string oAuthSessionToken, string keyword)
+        {
+            string roviSearchMovieByKeywordURL =
+                string.Format(
+                    "http://api.rovicorp.com/search/v2.1/video/search?entitytype=movie&query={0}&rep=1&facet=genre&size=1&offset=0&language=en&country=US&format=json&apikey={1}&sig={2}",
+                    keyword, RoviAppId, oAuthSessionToken );
+            //"http://sr-prod.rovicorp.com:8080/rovi-snr-ws-2/rest/phoenix_global/search?size=20&offset=0&modifiers=restriction%3bcount&types=CosmoMovie&fields=name&query=hanks";
+
+            string results = string.Empty;
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create( roviSearchMovieByKeywordURL );
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse( );
+
+                StreamReader sr = new StreamReader( resp.GetResponseStream( ) );
+                results = sr.ReadToEnd( );
+
+                sr.Close( );
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains( "400" ))
+                {
+                    //invalid reponse
+                }
+            }
+
+            JObject jo = JObject.Parse( results );
+            return jo;
+//            string response = requestRoviData( userLikeUrl );
+//            Console.WriteLine( response );
+            //ParseResponse( response );
+        }
+
+
+#region GetAccessToken
+        public static string GetAccessTokenFromCode(string AppId, string SecretKey)
+        {
+            //get the timestamp value
+            string timestamp = (DateTime.UtcNow - new DateTime( 1970, 1, 1, 0, 0, 0 )).TotalSeconds.ToString( );
+
+            //grab just the integer portion
+            timestamp = timestamp.Substring( 0, timestamp.IndexOf( "." ) );
+
+            //set the API key (note that this is not a valid key!
+            string apikey = RoviAppId;
+
+            //set the shared secret key
+            string secret = RoviAppSecretKey;
+
+            //call the function to create the hash
+            string sig = CreateMD5Hash( apikey + secret + timestamp );
+
+            return sig;
+        }
+
+        //note, requires "using System.Security.Cryptography;"
+        public static string CreateMD5Hash(string input)
+        {
+            // Use input string to calculate MD5 hash
+            MD5 md5 = MD5.Create( );
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes( input );
+            byte[] hashBytes = md5.ComputeHash( inputBytes );
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder( );
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append( hashBytes[i].ToString( "x2" ) );  //this will use lowercase letters, use "X2" instead of "x2" to get uppercase
+            }
+            return sb.ToString( );
+        }
+#endregion 
+
+    }
+}
